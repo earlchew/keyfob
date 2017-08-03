b3ea6026710dd1a2ce67789b3f4fe7655833222b
Earl Chew <earl_chew@yahoo.com>
2017-08-03 07:46:46 -0700

    [setup] Update to include long_description and classifiers
    Signed-off-by: Earl Chew <earl_chew@yahoo.com>

--- setup.py
+++ setup.py
@@ -4,26 +4,44 @@ from setuptools import setup, Extension
 
 PKGNAME = 'keysafe'.lower()
 
-setup(name=PKGNAME,
-      version='1.0',
-      description='Remember secrets for the duration of a login session',
-      url='https://github.com/earlchew/keysafe',
-      author='Earl Chew',
-      author_email='earl_chew@yahoo.com',
-      license='BSD-2-Clause',
-      packages=[PKGNAME],
-      entry_points={
-          'console_scripts': [
-              '{pkgname} = {pkgname}.__main__:main'.format(pkgname=PKGNAME),
-          ]},
-      package_dir={'' : 'lib'},
-      install_requires=['keyutils', 'cryptography'],
-      ext_package=PKGNAME,
-      ext_modules=[Extension(
-          'lib{}'.format(PKGNAME),
-          ['libkeysafe.c'],
-          define_macros=[
-              ('_GNU_SOURCE', None),
-              ('MODULE_NAME', PKGNAME.upper()),
-              ('MODULE_name', PKGNAME)],
-          extra_compile_args=['-std=c99'])])
+
+def readme():
+    with open('README.rst', 'r') as readmefile:
+        return readmefile.read()
+
+
+setup(
+    name=PKGNAME,
+    version='1.0',
+    description='Remember secrets for the duration of a login session',
+    long_description=readme(),
+    url='https://github.com/earlchew/keysafe',
+    author='Earl Chew',
+    author_email='earl_chew@yahoo.com',
+    license='BSD-2-Clause',
+    packages=[PKGNAME],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Console',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Security',
+        'Topic :: System :: Systems Administration',
+        'Topic :: System :: Shells',
+    ],
+    entry_points={
+        'console_scripts': [
+          '{pkgname} = {pkgname}.__main__:main'.format(pkgname=PKGNAME),
+        ]},
+    package_dir={'' : 'lib'},
+    install_requires=['keyutils', 'cryptography'],
+    ext_package=PKGNAME,
+    ext_modules=[Extension(
+        'lib{}'.format(PKGNAME),
+        ['libkeysafe.c'],
+        define_macros=[
+            ('_GNU_SOURCE', None),
+            ('MODULE_NAME', PKGNAME.upper()),
+            ('MODULE_name', PKGNAME)],
+        extra_compile_args=['-std=c99'])])
